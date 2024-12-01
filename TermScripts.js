8f1895d14072d99122451c6ba5cbf8236599a642
dallen98-web <dallen98@students.kennesaw.edu>
2024-12-01 15:43:16 -0500

    Add files via upload

--- TermScripts.js
+++ TermScripts.js
@@ -14,6 +14,22 @@ function WeatherData() {
 
 }
 
+function WeatherDataCel() {
+    var term = document.getElementById('textbox').value
+
+
+    $("#results").html("Displaying Results ...");
+    $.getJSON("https://api.openweathermap.org/data/2.5/forecast?q=" + term + ",US&cnt=4&appid=bb303870e85fb95152c243f1005830be&units=metric", function (jsonData) {
+        $("#results").html("");
+        var template = $('#weathertemplate').html();
+        var html = Mustache.render(template, jsonData);
+        $("#results").html(html);
+    });
+
+
+}
+
+
 function CurrentWeatherData() {
     var term = document.getElementById('textbox').value
 
